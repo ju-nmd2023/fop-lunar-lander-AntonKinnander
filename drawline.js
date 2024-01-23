0e376822058c095972d2150c760d8ee1419ce522
AntonKinnander <a.kinnander@icloud.com>
2024-01-23 18:33:59 +0100

    Create files and configure index.html

---
+++ drawline.js
@@ -0,0 +1,6 @@
+frameRate(600);
+noStroke();
+background(0);
+function draw() {
+  ellipse(mouseX, mouseY, 20);
+}
---
+++ lunarlander.js
@@ -0,0 +1,9 @@
+//Background
+background(0);
+
+for (let i = 0; i < 200, i++; ) {
+  let x = math.round(math.random() * i);
+  let y = math.round(math.random() * i);
+  let rad = 6 * math.random();
+  circle(x, y, rad);
+}
