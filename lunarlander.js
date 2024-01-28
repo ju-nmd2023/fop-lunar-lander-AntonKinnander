4061f4c5021329221911431f5dd20f9481b6ebfd
AntonKinnander <a.kinnander@icloud.com>
2024-01-28 09:39:03 +0100

    starry background
    Modified from creative coding with Maxims sunflower spiral

--- lunarlander.js
+++ lunarlander.js
@@ -1,9 +1,49 @@
-//Background
-background(0);
+/*
+  Tutorial by me :)
+    https://www.youtube.com/watch?v=RrSOv9FH6uo
 
-for (let i = 0; i < 200, i++; ) {
-  let x = math.round(math.random() * i);
-  let y = math.round(math.random() * i);
-  let rad = 6 * math.random();
-  circle(x, y, rad);
+  inspiration + explanation for golden ratio:
+    Numberphile - The Golden Ratio (why it is so irrational)
+    https://www.youtube.com/watch?v=sj8Sg8qnjOg
+*/
+
+function setup() {
+  const size = max(windowWidth, windowHeight);
+  createCanvas(size, size);
+  noStroke();
+}
+
+const radius = Math.sqrt(0.5);
+const dotSize = 0.004;
+const PHI = (1 + Math.sqrt(5)) / 2;
+
+const fillCol = [208, 103, 82];
+const bgCol = [16, 6, 6];
+
+function drawMoon() {}
+
+//altered and added to a tutorial by creative coding youtube
+function drawStars() {
+  scale(width, height);
+  background(bgCol);
+  fill(fillCol);
+
+  const count = 600;
+  for (let i = 1; i < count; i++) {
+    noLoop();
+    const f = i / count;
+    const angle = i * PHI;
+    const dist = f * radius;
+
+    let x = 0.5 + Math.random() / 10 + cos(angle * TWO_PI) * dist;
+    const y = 0.5 + Math.random() / 10 + sin(angle * TWO_PI) * dist;
+
+    let r = f * dotSize * (Math.random() * (0.8 + Math.random()));
+
+    circle(x, y, r);
+  }
+}
+
+function draw() {
+  drawStars();
 }
