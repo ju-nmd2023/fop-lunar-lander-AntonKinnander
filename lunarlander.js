177e6533ad797eadf3c3da861abf3db9608a241e
AntonKinnander <a.kinnander@icloud.com>
2024-02-08 11:43:57 +0100

    fix

--- lunarlander.js
+++ lunarlander.js
@@ -1,4 +1,4 @@
-const size = Math.max(windowWidth, windowHeight);
+let size;
 const radius = Math.sqrt(0.5);
 const dotSize = 0.004;
 const PHI = (1 + Math.sqrt(5)) / 2;
@@ -6,6 +6,7 @@ const fillCol = [208, 103, 82];
 const bgCol = [16, 6, 6];
 
 function setup() {
+  size = max(windowWidth, windowHeight);
   createCanvas(size, size);
   noStroke();
   background(bgCol);
@@ -13,7 +14,7 @@ function setup() {
 
 //altered and added to a tutorial by creative coding youtube
 function drawStars() {
-  scale(windowWidth, windowHeight);
+  scale(size, size);
 
   fill(fillCol);
 
@@ -30,14 +31,43 @@ function drawStars() {
     circle(x, y, r);
   }
 }
+function drawMoon() {
+  for (let i = 0; i < 9; i++) {
+    ellipse(0, 0, (height / 3) * f, height / 3);
+  }
+
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //ROSEGARDEN
+  //Jag bjud 90%
+  //ROSEGARDEN
+}
 
 function draw() {
   push();
   translate(size / 2, size / 2);
   stroke(fillCol);
+  // ellipse(0, 0, height / 3); nh
   fill(bgCol);
-  // ellipse(0, 0, height / 3);
-  sphere(0, 0, height / 3);
+  //Draw ellipse multiple lines
+  ellipse(0, 0, height / 3);
+  // ellipse(0, 0, height / 3, height / 4);
+  // ellipse(0, 0, height / 3, height / 5);
+  // ellipse(0, 0, height / 3, height / 6);
+  // ellipse(0, 0, height / 3, height / 7);
+  // ellipse(0, 0, height / 3, height / 8);
+  // ellipse(0, 0, height / 3, height / 9);
+  // ellipse(0, 0, height / 3, height / 10);
+  // ellipse(0, 0, height / 3, height / 11);
   pop();
   drawStars();
 }
