f7d3057ef03dfc19de27ce901dbcbd2997391a81
fiznjik <149686770+AntonKinnander@users.noreply.github.com>
2024-02-13 01:03:48 +0100

    UPDATE FOR PULL IN LKPG ON MAC

--- rotatestars.js
+++ rotatestars.js
@@ -14,11 +14,13 @@ let verticalSpeed = 0;
 let horizontalspeed = 0;
 
 function setup() {
+  translate(height, height);
   vpFactor = max(windowWidth, windowHeight) / min(windowWidth, windowHeight);
   createCanvas(windowWidth, windowHeight);
   noStroke();
   generateStars();
   background(bgCol);
+  rectMode(CENTER);
 }
 //Inspiration from  creative coding youtube-
 //Using Garrits code from weekly challenge 3
@@ -26,13 +28,14 @@ function setup() {
 function generateStars() {
   fill(fillCol);
   const count = 717;
+  stars.push({ x: width / 2, y: height / 2, r: 1000, alpha: 1 });
   for (let i = 1; i < count; i++) {
     const f = i / count;
     const angle = i * PHI;
     const dist = f * radius;
     const star = {
-      x: 0.5 + Math.random() / 10 + cos(angle * TWO_PI) * dist,
-      y: 0.5 + Math.random() / 10 + sin(angle * TWO_PI) * dist,
+      x: Math.random() / 10 + cos(angle * TWO_PI) * dist,
+      y: Math.random() / 10 + sin(angle * TWO_PI) * dist,
       r: f * starSize * (Math.random() * (0.8 + Math.random())),
       alpha: Math.random(),
     };
@@ -41,7 +44,7 @@ function generateStars() {
 }
 
 function drawStars() {
-  scale(width, height);
+  translate(height);
   for (let star of stars) {
     fill(fillCol, Math.abs(Math.sin(star.alpha)) * 255);
     if (width < height) {
@@ -50,4 +53,23 @@ function drawStars() {
       ellipse(star.x, star.y, star.r / vpFactor, star.r);
     }
   }
+}
+
+function draw() {
+  fill(255, 0, 0);
+  rect(width, height);
+  background(bgCol);
+  horizontalspeed += 0.01;
+  circle(width / 2, height / 2, radius);
+  push();
+  scale(width, height);
+  rotate(horizontalspeed);
+  push();
+
+  push();
+
+  drawStars();
+  pop();
+  pop();
+  pop();
 }
