c6433c2e934202a5b2a6831d38dfdcece78f9971
AntonKinnander <a.kinnander@icloud.com>
2024-02-15 18:17:28 +0100

    rewrite less cool starry sky but it rotates

--- count.js
+++ count.js
@@ -1,6 +1,20 @@
-let count = 0;
+function drawMoon() {
+  let d = 752;
+  strokeWeight(2);
+  stroke(255);
+  //No stars on the moon
+  push();
+  fill(2);
+  ellipse(0, 0, d / 9);
+  pop();
+  fill(0, 0);
 
-while (count < 5) {
-  count = count + 1;
-  console.log(count);
+  for (let i = 0; i < 3; i++) {
+    let f = 9 / Math.pow(i, -2);
+    ellipse(0, 0, d / 9 - f, d / f - f * 2.9);
+    ellipse(0, 0, d / 9 - f, d / f - f);
+  }
+}
+function draw() {
+  drawMoon();
 }
--- lunarlander copy.js
+++ lunarlander copy.js
@@ -44,8 +44,11 @@ class Star {
 }
 
 function drawMoon() {
-  let d = 752;
-  strokeWeight(10);
+  push();
+  scale(2.2);
+  let d = width;
+  strokeWeight(1);
+  stroke(fillCol);
   //No stars on the moon
   push();
   fill(bgCol);
@@ -58,10 +61,26 @@ function drawMoon() {
     ellipse(0, 0, d / 9 - f, d / f - f * 2.9);
     ellipse(0, 0, d / 9 - f, d / f - f);
   }
+  pop();
+}
+
+function drawRocket() {
+  push();
+  noStroke();
+  ellipse(0, height / 9 - 9 / Math.pow(12, -1), height / 10);
+  ellipse(0, height / 9 - 9 / Math.pow(16, -1), height / 10, height / 6);
+
+  for (let i = 2; i < 10; i++) {
+    let f = 9 / Math.pow(i, -1);
+    fill(0, 0);
+    ellipse(0, 0, height / 9 - f, height / f - f * 2.9);
+    ellipse(0, 0, height / 9 - f, height / f - f);
+  }
+  pop();
 }
 
 function draw() {
-  background(17, 7, 7, 127); // Slight transparency
+  // background(17, 7, 7, 127); // Slight transparency
   translate(width / 2, (3 * height) / 4);
   rotate(frameCount * 0.005);
   // Display and update each star
